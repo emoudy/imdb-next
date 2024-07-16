fb753fa85248cd19e6aa4eee1188025f3bbe3d0c
Elda Moudy <elda@moudy.net>
2024-07-16 14:03:04 -0400

    Created the About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non
+        dignissim dolor, eu interdum augue. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas. Proin
+        malesuada id justo sed hendrerit. Aliquam at dolor sollicitudin,
+        suscipit ipsum nec, venenatis eros. Nullam euismod dolor id viverra
+        pellentesque. Etiam nisl lectus, pretium non maximus non, finibus nec
+        magna. Nulla lectus augue, porta varius euismod et, porta id tortor.
+        Phasellus vitae tortor convallis, pellentesque mi quis, venenatis
+        sapien. Maecenas erat dui, egestas quis orci id, efficitur viverra
+        turpis. Morbi vitae tincidunt nunc, ac fermentum eros. Mauris lacus
+        erat, egestas eu consequat non, congue fringilla magna. Praesent viverra
+        fringilla diam, a pulvinar tortor pharetra varius.
+      </p>
+      <p>
+        Etiam nec varius mauris. Maecenas sollicitudin lobortis neque, ut
+        rhoncus lacus ultricies vel. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Donec placerat
+        auctor mollis. Mauris id libero quis sem faucibus consectetur. Donec
+        nisl sapien, mattis vitae semper dignissim, cursus et sem. Phasellus sit
+        amet vestibulum diam.
+      </p>
+      <p>
+        Praesent sed cursus dolor. Morbi vestibulum molestie lacus vel pretium.
+        Phasellus molestie felis eget lacus ultrices consequat. Duis rutrum quam
+        et tincidunt fermentum. Integer molestie tempus nulla id suscipit. Donec
+        pulvinar, lacus vel lacinia malesuada, dolor nibh pharetra urna,
+        fringilla viverra tellus purus at diam. Curabitur dapibus tortor neque,
+        sed venenatis diam feugiat vel. In hac habitasse platea dictumst.
+        Integer rhoncus bibendum dui, congue posuere nibh imperdiet id. Integer
+        dignissim metus id iaculis efficitur. Maecenas dignissim ultrices neque
+        ac finibus. Praesent nec porttitor velit. Vestibulum dolor quam, blandit
+        eget porta vel, bibendum id lectus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
