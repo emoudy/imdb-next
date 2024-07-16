c287852e1107c8e5dc3f5b4d9762540a4a89904b
Elda Moudy <elda@moudy.net>
2024-07-16 12:12:38 -0400

    Adding Header and MenuItems components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,10 +9,14 @@ export const metadata = {
   description: "This is a movie database clone",
 };
 
-export default function RootLayout({ children }) {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}</body>
     </html>
   );
 }
+
+export default RootLayout
