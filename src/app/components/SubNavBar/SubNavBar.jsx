bb24f686696704744b775007d8d1dc95b70cdd12
Elda Moudy <elda@moudy.net>
2024-07-17 13:26:43 -0400

    Adding Suspense for useSearchParams

--- src/app/components/SubNavBar/SubNavBar.jsx
+++ src/app/components/SubNavBar/SubNavBar.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import SubNavBarItem from "./SubNavBarItem";
 
 export default function SubNavBar() {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-      <SubNavBarItem title="Trending" param="trending" />
-      <SubNavBarItem title="Top Rated" param="top_rated" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SubNavBarItem title="Trending" param="trending" />
+        <SubNavBarItem title="Top Rated" param="top_rated" />
+      </Suspense>
     </div>
   );
 }
