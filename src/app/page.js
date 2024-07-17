67677ce7b55b66a5adfc4b5a6288a8a6f6c8753b
Elda Moudy <elda@moudy.net>
2024-07-17 01:14:41 -0400

    Loading component

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,24 @@ import MovieResults from "./components/MovieResults";
 
 const API_KEY = process.env.API_KEY;
 
-const Home = async({ searchParams }) => {
-  const genre = searchParams.genre || 'trending';
-  const category = genre === 'trending' ? 'trending/all/week' : `movie/top_rated`;
-  const res = await fetch(`https://api.themoviedb.org/3/${category}?api_key=${API_KEY}&language=en-US&page=1`);
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "trending";
+  const category = genre === "trending" ? "trending/all/week" : "movie/top_rated";
+  const getMoviesURL = `https://api.themoviedb.org/3/${category}?api_key=${API_KEY}&language=en-US&page=1`;
+
+  const res = await fetch(getMoviesURL, {next: { revalidate: 600 }})
   const data = await res.json();
 
   if (!res.ok) {
-    // return <div>Failed to load</div>;
-    throw new Error('Failed to fetch data');
+    // This will activate the closest `error.jsx` Error Boundary
+    throw new Error('Failed to fetch data')
   }
 
-  const results = data.results;
-  console.log(results);
+  const movies = data.results;
 
   return (
     <div>
-      <MovieResults results={results} />
+      <MovieResults results={movies} />
     </div>
   )
-}
-
-export default Home;
+};
