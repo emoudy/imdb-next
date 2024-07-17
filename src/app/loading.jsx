72eba68a3709a108c1fa1ef3f01f674a5e21a8da
Elda Moudy <elda@moudy.net>
2024-07-16 22:39:52 -0400

    spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center mt-16">
+      <image className="h-52" src="spinner.svg" alt="loading..."></image>
+    </div>
+  );
+};
+
+export default loading;
