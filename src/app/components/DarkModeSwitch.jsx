d560a03ceeb4f3483ae4e816bd172d1afe3fbf49
Elda Moudy <elda@moudy.net>
2024-07-17 13:03:05 -0400

    making a toggleTheme function

--- src/app/components/DarkModeSwitch.jsx
+++ src/app/components/DarkModeSwitch.jsx
@@ -5,13 +5,13 @@ import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 export default function DarkModeSwitch() {
-  const { theme, setTheme, resolvedTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
   const dark = "dark";
   const light = "light";
 
   // To be able to capture the theme from the system, we need to wait for
   // the component to be mounted. Otherwise, the theme will load to the default theme
-  // and it could be incorrect if the user is has dark mode enabled on their system.
+  // and it could be incorrect if the user uses dark mode on their system.
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -23,9 +23,13 @@ export default function DarkModeSwitch() {
     return null;
   }
 
+  const toggleTheme = () => {
+    setTheme(resolvedTheme === dark ? light : dark);
+  };
+
   return (
     <div
-      onClick={() => setTheme(theme === dark ? light : dark)}
+      onClick={toggleTheme}
       className="text-xl cursor-pointer hover:text-amber-500"
     >
       {resolvedTheme === dark ? <MdDarkMode /> : <MdLightMode />}
